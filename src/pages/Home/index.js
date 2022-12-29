dab7d8b928172d8f9a6cb837698fbfd5608132e9
Leonardo de Souza <leonardo.souza.ramos@hotmail.com>
2022-12-29 02:28:33 -0300

    Só update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,43 @@
-import React from "react";
-import {Text, View} from "react-native";
+import React, { useContext, useState } from "react";
+import {Button, Text, View} from "react-native";
+import {AuthContext} from "../../contexts/auth";
+import Header from "../../components/Header";
+import {Backgroud,Container,Nome,Saldo,Title, List} from './style'
+import HistoricoList from "../../components/HistoricoList";
 
 export default function Home(){
+  const [historico,setHistorico] = useState([
+    {key: '1', tipo: 'receita', valor: 1200},
+    {key: '2', tipo: 'despesa', valor: 200},
+    {key: '3', tipo: 'receita', valor: 40},
+    {key: '4', tipo: 'receita', valor: 89.68},
+    {key: '5', tipo: 'despesa', valor: 500.00},
+    {key: '6', tipo: 'despesa', valor: 310.00},
+
+  ])
+
+
+    const {user} = useContext(AuthContext)
+
     return(
-        <View>
-            <Text>Home</Text>
-        </View>
+      <Backgroud>
+        <Header/>
+        <Container>
+          <Nome>Matheus</Nome>
+          <Saldo>R$123,00</Saldo>
+
+        </Container>
+
+        <Title>Ultima movimentações</Title>
+
+        <List
+          showsVerticalScrollIndicator={false}
+          data={historico}
+          keyExtractor={item => item.key}
+          renderItem={({item}) => (<HistoricoList data={item} />) }
+
+        />
+
+      </Backgroud>
     )
 }
--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,16 +1,38 @@
+import 'react-native-gesture-handler';
 import React from "react";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {createDrawerNavigator} from "@react-navigation/drawer";
 
 import Home from "../pages/Home";
+import New from "../pages/New"
+import Profile from "../pages/Profile";
 
-const AppStack = createNativeStackNavigator();
+const AppDrawer = createDrawerNavigator();
 
 function AppRoutes(){
     return(
-        <AppStack.Navigator>
-            <AppStack.Screen name="Home" component={Home}/>
-        </AppStack.Navigator>
+        <AppDrawer.Navigator
+
+          screenOptions={{
+
+            drawerStyle: {
+              fontWeight: 'bold',
+              backgroundColor: '#171717',
+
+            },
+            drawerActiveTintColor:'#FFF',
+            drawerActiveBackgroundColor: '#00b94a',
+            drawerInactiveTintColor: '#DDD',
+            drawerItemStyle:{
+              marginVertical: 5,
+            }
+
+
+          }}
+        >
+            <AppDrawer.Screen name="Home" component={Home} options={{headerShown: false}}/>
+            <AppDrawer.Screen name="Registrar" component={New} options={{headerShown: false}}/>
+            <AppDrawer.Screen name="Perfil" component={Profile} options={{headerShown: false}}/>
+        </AppDrawer.Navigator>
     )
 }
-
 export default AppRoutes
